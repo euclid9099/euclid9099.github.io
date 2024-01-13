245144c9a4675bf5fff53e502f8cbee166f0c3e8
euclid9099 <87277722+euclid9099@users.noreply.github.com>
2024-01-13 14:42:17 +0100

    add and style links

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.info('main.js loaded');
+const config = {
+    links: {
+        discord: 'https://example.com/discord',
+        github: 'https://github.com/euclid9099',
+        mail: 'mailto:mail@example.com',
+        issues: 'https://github.com/euclid9099/portfolio/issues/new'
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('a').forEach((link) => {
+        if (!link.getAttribute('href')?.startsWith('config:')) return;
+
+        link.setAttribute('href', config.links[link.getAttribute('href').split(':')[1]]);
+    });
+});
